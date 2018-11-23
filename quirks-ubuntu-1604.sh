afd46814dce352653c0326e5bf3765052118f83c
MCMrARM <mcmrarm@gmail.com>
2018-11-23 12:31:33 +0100

    Ubuntu 16.04: add qt rpath

--- quirks-ubuntu-1604.sh
+++ quirks-ubuntu-1604.sh
@@ -1,5 +1,6 @@
 quirk_build_msa() {
   add_cmake_options -DDEB_XENIAL_DEPENDENCIES=ON
+  add_cmake_options -DQT_RPATH=/opt/qt59/lib/
 }
 quirk_build_mcpelauncher() {
   add_cmake_options -DDEB_XENIAL_DEPENDENCIES=ON
@@ -7,5 +8,6 @@ quirk_build_mcpelauncher() {
 }
 quirk_build_mcpelauncher_ui() {
   add_cmake_options -DDEB_XENIAL_DEPENDENCIES=ON
+  add_cmake_options -DCMAKE_INSTALL_RPATH=/opt/qt59/lib/
 }
 
