6b405afd52c127ec7eaa2a50b0f443e04416d739
MCMrARM <mcmrarm@gmail.com>
2019-02-23 12:22:51 +0100

    Move build code between build.sh and common.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-SOURCE_DIR=${PWD}/source
-BUILD_DIR=${PWD}/build
-OUTPUT_DIR=${PWD}/output
+source common.sh
 
-MAKE_JOBS=$(nproc)
 QUIRKS_FILE=
 
 while getopts "h?q:j:" opt; do
@@ -22,81 +19,15 @@ while getopts "h?q:j:" opt; do
     esac
 done
 
-COLOR_STATUS=$'\033[1m\033[32m'
-COLOR_RESET=$'\033[0m'
+load_quirks "$QUIRKS_FILE"
 
-show_status() {
-  echo "$COLOR_STATUS=> $1$COLOR_RESET"
-}
-check_run() {
-  $@
-  local STATUS=$?
-  if (( $STATUS != 0 )); then
-    exit $STATUS
-  fi
-}
-
-shopt -s nullglob
-
-if [ ! -z "$QUIRKS_FILE" ]; then
-  show_status "Loading quirks file: $QUIRKS_FILE"
-  source $QUIRKS_FILE
-fi
-
-call_quirk() {
-  local QUIRK_NAME="quirk_$1"
-  QUIRK_NAME=`declare -f -F "$QUIRK_NAME"`
-  if (( $? == 0 )); then
-    show_status "Executing $QUIRK_NAME"
-    $QUIRK_NAME
-  fi
-}
-
-mkdir -p $SOURCE_DIR
-mkdir -p $BUILD_DIR
-mkdir -p $OUTPUT_DIR
-
+create_build_directories
 call_quirk init
 
 show_status "Downloading sources"
-
-download_repo() {
-  show_status "Downloading $2"
-  if [ -d $SOURCE_DIR/$1 ]; then
-    pushd $SOURCE_DIR/$1
-    check_run git pull
-    check_run git submodule update
-    popd
-  else
-    check_run git clone --recursive $2 $SOURCE_DIR/$1
-  fi
-}
-
 download_repo msa https://github.com/minecraft-linux/msa-manifest.git
 download_repo mcpelauncher https://github.com/minecraft-linux/mcpelauncher-manifest.git
 download_repo mcpelauncher-ui https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git
-
-reset_cmake_options() {
-  CMAKE_OPTIONS=
-}
-add_cmake_options() {
-  CMAKE_OPTIONS="$CMAKE_OPTIONS $@"
-}
-build_component() {
-  show_status "Building $1"
-  mkdir -p $BUILD_DIR/$1
-  pushd $BUILD_DIR/$1
-  echo "cmake" $CMAKE_OPTIONS "$SOURCE_DIR/$1"
-  check_run cmake $CMAKE_OPTIONS "$SOURCE_DIR/$1"
-  check_run make -j${MAKE_JOBS}
-  popd
-  pushd $OUTPUT_DIR
-  for cf in $BUILD_DIR/$1/**/CPackConfig.cmake; do
-    echo "CPack config: $cf"
-    check_run cpack --config $cf
-  done
-  popd
-}
 
 call_quirk build_start
 
@@ -111,4 +42,4 @@ reset_cmake_options
 call_quirk build_mcpelauncher_ui
 build_component mcpelauncher-ui
 
-rm -rf $OUTPUT_DIR/_CPack_Packages
+cleanup_build
---
+++ common.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+SOURCE_DIR=${PWD}/source
+BUILD_DIR=${PWD}/build
+OUTPUT_DIR=${PWD}/output
+
+MAKE_JOBS=$(nproc)
+
+COLOR_STATUS=$'\033[1m\033[32m'
+COLOR_RESET=$'\033[0m'
+
+show_status() {
+  echo "$COLOR_STATUS=> $1$COLOR_RESET"
+}
+check_run() {
+  $@
+  local STATUS=$?
+  if (( $STATUS != 0 )); then
+    exit $STATUS
+  fi
+}
+
+shopt -s nullglob
+
+load_quirks() {
+  if [ ! -z "$1" ]; then
+    show_status "Loading quirks file: $1"
+    source "$1"
+  fi
+}
+
+call_quirk() {
+  local QUIRK_NAME="quirk_$1"
+  QUIRK_NAME=`declare -f -F "$QUIRK_NAME"`
+  if (( $? == 0 )); then
+    show_status "Executing $QUIRK_NAME"
+    $QUIRK_NAME
+  fi
+}
+
+create_build_directories() {
+  mkdir -p $SOURCE_DIR
+  mkdir -p $BUILD_DIR
+  mkdir -p $OUTPUT_DIR
+}
+
+download_repo() {
+  show_status "Downloading $2"
+  if [ -d $SOURCE_DIR/$1 ]; then
+    pushd $SOURCE_DIR/$1
+    check_run git pull
+    check_run git submodule update
+    popd
+  else
+    check_run git clone --recursive $2 $SOURCE_DIR/$1
+  fi
+}
+
+reset_cmake_options() {
+  CMAKE_OPTIONS=
+}
+add_cmake_options() {
+  CMAKE_OPTIONS="$CMAKE_OPTIONS $@"
+}
+build_component() {
+  show_status "Building $1"
+  mkdir -p $BUILD_DIR/$1
+  pushd $BUILD_DIR/$1
+  echo "cmake" $CMAKE_OPTIONS "$SOURCE_DIR/$1"
+  check_run cmake $CMAKE_OPTIONS "$SOURCE_DIR/$1"
+  check_run make -j${MAKE_JOBS}
+  popd
+  pushd $OUTPUT_DIR
+  for cf in $BUILD_DIR/$1/**/CPackConfig.cmake; do
+    echo "CPack config: $cf"
+    check_run cpack --config $cf
+  done
+  popd
+}
+
+cleanup_build() {
+  rm -rf $OUTPUT_DIR/_CPack_Packages
+}
