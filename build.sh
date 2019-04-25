df9a139f65081b3861db61631ff0f2c3febbc1ac
MCMrARM <mcmrarm@gmail.com>
2019-04-25 16:10:40 +0200

    Initial work on supporting disco

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 source common.sh
 
 QUIRKS_FILE=
+OS_NAME=
 
 while getopts "h?q:j:" opt; do
     case "$opt" in
@@ -10,11 +11,14 @@ while getopts "h?q:j:" opt; do
         echo "build.sh"
         echo "-j  Specify the number of jobs (the -j arg to make)"
         echo "-q  Specify the quirks file"
+        echo "-o  Specify the OS name"
         exit 0
         ;;
     j)  MAKE_JOBS=$OPTARG
         ;;
     q)  QUIRKS_FILE=$OPTARG
+        ;;
+    o)  OS_NAME=$OPTARG
         ;;
     esac
 done
@@ -33,14 +37,17 @@ call_quirk build_start
 
 reset_cmake_options
 add_cmake_options -DENABLE_MSA_QT_UI=ON -DMSA_UI_PATH_DEV=OFF
+add_cmake_options -DDEB_OS_NAME=$OS_NAME
 call_quirk build_msa
 build_component msa
 install_component_cpack msa
 reset_cmake_options
+add_cmake_options -DDEB_OS_NAME=$OS_NAME
 call_quirk build_mcpelauncher
 build_component mcpelauncher
 install_component_cpack mcpelauncher
 reset_cmake_options
+add_cmake_options -DDEB_OS_NAME=$OS_NAME
 call_quirk build_mcpelauncher_ui
 build_component mcpelauncher-ui
 install_component_cpack mcpelauncher-ui
