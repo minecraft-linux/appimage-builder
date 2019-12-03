d54181a81b33cf95cc60301c4cb484ff5ba651b5
Christopher Homberger <christopher.homberger@web.de>
2019-12-03 23:02:17 +0100

    Add bionic pure 32 bit

---
+++ quirks-32.sh
@@ -0,0 +1,9 @@
+quirk_build_msa() {
+  add_cmake_options -DCMAKE_C_FLAGS=-m32
+}
+quirk_build_mcpelauncher() {
+  add_cmake_options -DUSE_OWN_CURL=ON -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+}
+quirk_build_mcpelauncher_ui() {
+  add_cmake_options -DUSE_OWN_CURL=ON -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DCMAKE_C_FLAGS=-m32
+}
