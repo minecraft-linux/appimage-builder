d77726bb1ae762ffa46b807cb1825c548bdd4b0c
Christopher Homberger <christopher.homberger@web.de>
2019-12-04 00:06:21 +0100

    Test2

--- quirks-ubuntu-1604-32.sh
+++ quirks-ubuntu-1604-32.sh
@@ -1,10 +1,10 @@
 quirk_build_msa() {
-  add_cmake_options -DQT_RPATH=/opt/qt59/lib/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DQT_RPATH=/opt/qt59/lib/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DCMAKE_FIND_ROOT_PATH=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher() {
-  add_cmake_options -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DCMAKE_FIND_ROOT_PATH=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher_ui() {
-  add_cmake_options -DCMAKE_INSTALL_RPATH=/opt/qt59/lib/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DLIBZIP_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DCMAKE_INSTALL_RPATH=/opt/qt59/lib/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DCMAKE_FIND_ROOT_PATH=/usr/lib/i386-linux-gnu/
 }
 
