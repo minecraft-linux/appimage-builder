0738ba7077055d6fec0640fd13a0f140fce0c399
Christopher Homberger <christopher.homberger@web.de>
2019-12-03 23:42:11 +0100

    Fix some errors

--- quirks-32.sh
+++ quirks-32.sh
@@ -1,9 +1,9 @@
 quirk_build_msa() {
-  add_cmake_options -DCMAKE_C_FLAGS=-m32 -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DCMAKE_CXX_FLAGS=-m32
+  add_cmake_options -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher() {
-  add_cmake_options -DUSE_OWN_CURL=ON -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher_ui() {
-  add_cmake_options -DUSE_OWN_CURL=ON -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5
+  add_cmake_options -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
