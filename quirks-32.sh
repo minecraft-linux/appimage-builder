335b791cd647c907d3828cbc2a034f5dfa38b282
Christopher Homberger <christopher.homberger@web.de>
2019-12-03 23:54:08 +0100

    Some fixes

--- quirks-32.sh
+++ quirks-32.sh
@@ -1,9 +1,9 @@
 quirk_build_msa() {
-  add_cmake_options -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DCMAKE_C_FLAGS="-m32 -Wl,-latomic" -DCMAKE_CXX_FLAGS="-m32 -Wl,-latomic" -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher() {
-  add_cmake_options -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DCMAKE_C_FLAGS="-Wl,-latomic" -DCMAKE_CXX_FLAGS="-Wl,-latomic" -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher_ui() {
-  add_cmake_options -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DCMAKE_C_FLAGS="-m32 -Wl,-latomic" -DCMAKE_CXX_FLAGS="-m32 -Wl,-latomic" -DQt5_DIR=/usr/lib/i386-linux-gnu/cmake/Qt5 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DLIBZIP_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
--- quirks-ubuntu-1604-32.sh
+++ quirks-ubuntu-1604-32.sh
@@ -5,6 +5,6 @@ quirk_build_mcpelauncher() {
   add_cmake_options -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher_ui() {
-  add_cmake_options -DCMAKE_INSTALL_RPATH=/opt/qt59/lib/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DCMAKE_INSTALL_RPATH=/opt/qt59/lib/ -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/ -DLIBZIP_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 
