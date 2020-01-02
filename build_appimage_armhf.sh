cad222f3c382b5f59b2db44d5d3da8cd98538f4e
Christopher Homberger <christopher.homberger@web.de>
2020-01-03 00:23:09 +0100

    Fix deb2appimage config name

--- build_appimage_armhf.sh
+++ build_appimage_armhf.sh
@@ -86,7 +86,7 @@ install_component mcpelauncher-ui mcpelauncher-ui-qt
 show_status "Packaging"
 pushd ${SOURCE_DIR}/mcpelauncher-deb2appimage
 check_run ./updatearmhf.sh
-${SOURCE_DIR}/deb2appimage/deb2appimage.sh -j config-bionic-armhf.json -o ${OUTPUT_DIR}
+${SOURCE_DIR}/deb2appimage/deb2appimage.sh -j config-buster-armhf.json -o ${OUTPUT_DIR}
 popd
 
 cleanup_build
