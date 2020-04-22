d51843df4361a93e10001f5596635c1685ed67d6
Christopher Homberger <christopher.homberger@web.de>
2020-04-23 00:44:13 +0200

    Fix old cmake version add compat for old glibc

---
+++ compat.h
@@ -0,0 +1,1 @@
+#define gettid() 0
--- quirks-ubuntu-1604.sh
+++ quirks-ubuntu-1604.sh
@@ -2,7 +2,7 @@ quirk_build_msa() {
   add_cmake_options -DQT_RPATH=/opt/qt59/lib/
 }
 quirk_build_mcpelauncher() {
-  add_cmake_options -DUSE_OWN_CURL=ON -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
+  add_cmake_options -DCMAKE_C_FLAGS="-include $PWD/compat.h" -DCMAKE_CXX_FLAGS="-include $PWD/compat.h"
 }
 quirk_build_mcpelauncher_ui() {
   add_cmake_options -DCMAKE_INSTALL_RPATH=/opt/qt59/lib/
