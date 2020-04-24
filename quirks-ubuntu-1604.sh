56865bde85212682bfdc70ed1364034dc1ac24b1
Christopher Homberger <christopher.homberger@web.de>
2020-04-24 20:28:44 +0200

    Uninstall x64 first

--- quirks-ubuntu-1604.sh
+++ quirks-ubuntu-1604.sh
@@ -5,7 +5,8 @@ quirk_build_mcpelauncher() {
   add_cmake_options -DZLIB_LIBRARY=/usr/lib/x86_64-linux-gnu/libz.so -DPNG_LIBRARY=/usr/lib/x86_64-linux-gnu/libpng.so -DPNG_PNG_INCLUDE_DIR=/usr/include/libpng -DX11_X11_LIB=/usr/lib/x86_64-linux-gnu/libX11.so -DCURL_LIBRARY=/usr/lib/x86_64-linux-gnu/libcurl.so -DCMAKE_CXX_FLAGS=-stdlib=libc++ -DBUILD_FAKE_JNI_TESTS=OFF -DBUILD_FAKE_JNI_EXAMPLES=OFF
 }
 quirk_build_mcpelauncher32() {
-  DEBIAN_FRONTEND=noninteractive apt -y install libc++-10-dev libc++abi-10-dev libc++-10-dev:i386 libc++abi-10-dev:i386
+  DEBIAN_FRONTEND=noninteractive apt -y purge libc++-10-dev libc++abi-10-dev
+  DEBIAN_FRONTEND=noninteractive apt -y install libc++-10-dev:i386 libc++abi-10-dev:i386
   add_cmake_options -DCMAKE_ASM_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS="-m32 -stdlib=libc++" -DCMAKE_CXX_COMPILER_TARGET="i686-linux-gnu" -DBUILD_FAKE_JNI_TESTS=OFF -DBUILD_FAKE_JNI_EXAMPLES=OFF -DUSE_OWN_CURL=ON -DOPENSSL_ROOT_DIR=/usr/lib/i386-linux-gnu/
 }
 quirk_build_mcpelauncher_ui() {
