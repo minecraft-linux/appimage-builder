7adfcbeeb88122deac36970c610a381b60ca77f2
Christopher Homberger <christopher.homberger@web.de>
2020-11-17 17:07:11 +0100

    Fix msa branch

--- build_appimage.sh
+++ build_appimage.sh
@@ -41,7 +41,7 @@ mkdir -p ${APP_DIR}
 call_quirk init
 
 show_status "Downloading sources"
-download_repo msa https://github.com/minecraft-linux/msa-manifest.git ng
+download_repo msa https://github.com/minecraft-linux/msa-manifest.git master
 download_repo mcpelauncher https://github.com/minecraft-linux/mcpelauncher-manifest.git ng
 download_repo mcpelauncher-ui https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git ng
 mkdir -p "$SOURCE_DIR/mcpelauncher-ui/lib/AppImageUpdate"
