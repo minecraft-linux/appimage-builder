aa8ad72c85e9c4fe3720872b326ecc926890624a
ChristopherHX <christopher.homberger@web.de>
2022-10-09 13:04:23 +0200

    [no ci] delete file

--- .circleci/deploy-debs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-shopt -s nullglob
-for f in $3/*.deb; do
-  echo "Uploading: $f"
-  curl -X POST -u "ci:$WEBSITE_CI_UPLOAD_PASS" --data-binary @"$f" -H "X-Type: $1" -H "X-Codename: $2" https://mcpelauncher.mrarm.io/api/v1/upload
-  echo ""
-done
