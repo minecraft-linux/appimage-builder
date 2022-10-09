ac20ab4c04a2c5bfb6440a74ca01439cb75d2275
ChristopherHX <christopher.homberger@web.de>
2022-10-09 13:04:50 +0200

    [no ci] delete file

--- quirks.sh
+++
@@ -1,4 +0,0 @@
-CFLAGS32="-I ${PWD}/copenssl32/include -Wl,-L$PWD/copenssl32/lib $CFLAGS32"
-CFLAGS="-I ${PWD}/copenssl64/include -Wl,-L$PWD/copenssl64/lib $CFLAGS"
-CXXFLAGS32="-stdlib=libc++ $CXXFLAGS32"
-CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
