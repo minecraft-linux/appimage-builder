9802bea31a6a28336f14ed75ba2cb115da540f38
ChristopherHX <christopher.homberger@web.de>
2023-11-16 02:10:49 +0100

    Update quirks-bookworm.sh

--- quirks-bookworm.sh
+++ quirks-bookworm.sh
@@ -1,10 +1,10 @@
 quirk_build_msa() {
-  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so" "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6" "-DQt6GuiTools_DIR/usr/lib/${DEBIANTARGET}/cmake/Qt6GuiTools"
+  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so" "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6" "-DQt6GuiTools_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6GuiTools"
 }
 quirk_build_mcpelauncher() {
-  add_cmake_options "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6" "-DQt6GuiTools_DIR/usr/lib/${DEBIANTARGET}/cmake/Qt6GuiTools"
+  add_cmake_options "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6" "-DQt6GuiTools_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6GuiTools"
 }
 quirk_build_mcpelauncher_ui() {
-  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so" "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6" "-DQt6GuiTools_DIR/usr/lib/${DEBIANTARGET}/cmake/Qt6GuiTools"
+  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so" "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6" "-DQt6GuiTools_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6GuiTools"
 }
 
