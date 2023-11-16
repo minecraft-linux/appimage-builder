c4d4fda2a69b0d64e7b113d1a8d4901f5967d74e
ChristopherHX <christopher.homberger@web.de>
2023-11-16 01:46:16 +0100

    Qt6_DIR

--- quirks-bookworm.sh
+++ quirks-bookworm.sh
@@ -1,9 +1,10 @@
 quirk_build_msa() {
-  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so"
+  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so" "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6"
 }
 quirk_build_mcpelauncher() {
+  add_cmake_options "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6"
 }
 quirk_build_mcpelauncher_ui() {
-  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so"
+  add_cmake_options "-DCURL_LIBRARY=/usr/lib/${DEBIANTARGET}/libcurl.so" "-DQt6_DIR=/usr/lib/${DEBIANTARGET}/cmake/Qt6"
 }
 
